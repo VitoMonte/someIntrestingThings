3e299b1a63cdc74bdd043decd957e358c3136cf2
VitoMonte <vitaliy.kaz@mail.ru>
2017-12-26 14:25:52 +0400

    new commit

--- getKeywords.php
+++ getKeywords.php
@@ -1,12 +1,15 @@
-protected function getKeyWords($article, $repits, $length) {        
+<?php
+
+function getKeyWords($article, $repits, $length)
+{          
         $text = explode(" ", strip_tags($article));
         $pieces = array_count_values($text);
         $keyword = '';
+        
         foreach ($pieces as $key => $value) {
-            if (iconv_strlen($key) > $length && $value > $repits) {
-                $keyword = $keyword . ' ' .$key;
-            } 
+                if (iconv_strlen($key) > $length && $value > $repits) {
+                        $keyword = $keyword . ' ' .$key;
+                } 
         }
-             
         return $keyword;
-    }
+}
