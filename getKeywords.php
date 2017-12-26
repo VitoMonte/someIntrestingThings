c1735a152d31a471b388e05715752ee87fc3aec9
VitoMonte <vitaliy.kaz@mail.ru>
2017-12-26 14:23:30 +0400

    newfile

---
+++ getKeywords.php
@@ -0,0 +1,12 @@
+protected function getKeyWords($article, $repits, $length) {        
+        $text = explode(" ", strip_tags($article));
+        $pieces = array_count_values($text);
+        $keyword = '';
+        foreach ($pieces as $key => $value) {
+            if (iconv_strlen($key) > $length && $value > $repits) {
+                $keyword = $keyword . ' ' .$key;
+            } 
+        }
+             
+        return $keyword;
+    }
